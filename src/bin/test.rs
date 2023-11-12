2507b09440a45c220ae2f7d9f6c4a057fe12e553
Mats Liljegren <liljegren.mats@gmail.com>
2023-11-12 20:55:37 +0100

    Added functional testing

---
+++ src/bin/test.rs
@@ -0,0 +1,147 @@
+/// Binary for running functional tets.
+///
+/// Each command talks to a specific module. Options given before
+/// command are generic options that works for all commands, while
+/// options given after the command are specific to that command.
+
+use topal::streamreader::StreamReader;
+use topal::parseable::{Parseable, ParseError};
+use clap::{Parser, Subcommand, command, arg};
+use url::Url;
+use tracing::{instrument, info};
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber;
+use std::collections::HashMap;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+/// Functional test binary
+struct App {
+    #[arg(long, short, value_parser(valid_verbose), default_value("off"), env("TOPAL_LOG_LEVEL"))]
+    verbose: LevelFilter,
+
+    #[command(subcommand)]
+    cmd: Command,
+}
+
+fn valid_verbose(opt: &str) -> Result<LevelFilter, String> {
+    let verbose_values = HashMap::from([
+        ("off", LevelFilter::OFF),
+        ("error", LevelFilter::ERROR),
+        ("warning", LevelFilter::WARN),
+        ("info", LevelFilter::INFO),
+        ("debug", LevelFilter::DEBUG),
+        ("trace", LevelFilter::TRACE),
+    ]);
+
+    let lower_opt = opt.to_lowercase();
+    match verbose_values.get(&*lower_opt) {
+        Some(value) => Ok(value.clone()),
+        None => Err(format!("{}: Not a valid verbose name, use '-h' for help", opt)),
+    }
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    /// Test StreamReader object.
+    ///
+    /// StreamReader parses stdin if path is '-', a URL if path is
+    /// a valid URL or a file name. It parses the stream character
+    /// by character and updates line and column position.
+    Streamreader {
+        #[arg(required(true))]
+        path: String,
+    },
+}
+
+#[instrument]
+fn cmd_streamreader(path: String) {
+    let mut streamreader = if path == "-" {
+        Ok(StreamReader::from_stdin())
+    } else {
+        match Url::parse(&path) {
+            // Could be parsed as URL, assume it is
+            Ok(_) => StreamReader::from_url(&path),
+            // Not an URL, assume it's a file path
+            Err(_) => StreamReader::from_path(&path),
+        }
+    }.unwrap();
+
+    println!("{:#?}", streamreader);
+    loop {
+        let ret = streamreader.take();
+        match ret {
+            Err(ParseError::EOS) => break,
+            Err(ParseError::Broken(why)) => {
+                println!("ERROR: {}", why);
+                break;
+            },
+            Ok(c) => print!("{}", c),
+        }
+    }
+    println!("{:#?}", streamreader);
+}
+
+/// Main function
+fn main() {
+//    let default_level:usize = Level::ERROR.into();
+    let args = App::parse();
+    println!("{:?}", args);
+    let subscriber = tracing_subscriber::fmt()
+        .compact()
+        .with_file(true)
+        .with_line_number(true)
+        .with_thread_ids(true)
+        .with_target(false)
+        .with_max_level(args.verbose)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+
+    info!("Testing tracing");
+
+    match args.cmd {
+        Command::Streamreader { path } => cmd_streamreader(path),
+    }
+}
+
+#[cfg(test)]
+
+// Run the function tests
+mod test {
+    use super::*;
+    use assert_cmd;
+
+    #[test]
+    fn stdin() {
+        let mut cmd = assert_cmd::Command::cargo_bin("test").unwrap();
+        let run = cmd
+            .arg("streamreader")
+            .arg("-")
+            .write_stdin("a\nbc")
+            .assert();
+        run.success();
+    }
+
+    #[test]
+    fn url() {
+        let mut cmd = assert_cmd::Command::cargo_bin("test").unwrap();
+        let run = cmd
+            .arg("streamreader")
+            .arg("https://example.com")
+            .assert();
+        run.success();
+    }
+
+    #[test]
+    fn file() {
+        let mut cmd = assert_cmd::Command::cargo_bin("test").unwrap();
+        let run = cmd
+            .arg("streamreader")
+            .arg("./README.rst")
+            .assert();
+        run.success();
+    }
+
+}
+
